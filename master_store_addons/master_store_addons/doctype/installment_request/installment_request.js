69854d9b3ca1a0e4ec89c8ddd90995fea21a4a4c
etms <etms@Ubunto22.04.1.myguest.virtualbox.org>
2023-02-02 06:04:58 -0800

    adde price list when the customer is selected and give the last price for every item with thier ammount

---
+++ master_store_addons/master_store_addons/doctype/installment_request/installment_request.js
@@ -0,0 +1,109 @@
+// Copyright (c) 2023, ebkar.ly and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Installment Request', {
+	refresh: function(frm) {
+		console.log('imad told me to do it')
+		frm.set_query('price_list', function(){
+			return {
+				filters: {
+					selling: 1
+				}
+			}
+		})
+	},
+	price_list: async function(frm) {
+		console.log('asd');
+		for(let i = 0; i < frm.doc.installment_request_table.length; i++) {
+			let row = frm.doc.installment_request_table[i];
+
+			let data = await frappe.call(
+				{
+					method: "frappe.client.get_value",
+					args: {
+						doctype: "Item Price",
+						fieldname: "price_list_rate",
+						filters: {
+							price_list: frm.doc.price_list,
+							item_code:	row.item_code
+						}
+					},
+					async: true
+				}
+			)
+
+			if(data.message) {
+				
+				console.log(row.rate, data.message.price_list_rate);
+				row.rate = data.message.price_list_rate;
+
+				row.amount = row.qty * row.rate
+
+			}
+		}
+		frm.refresh_fields()
+
+	}
+});
+
+frappe.ui.form.on('Installment Request Table', {
+	item_code: function(frm, cdt, cdn){
+		let d = locals[cdt][cdn]
+		frappe.call(
+			{
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Item Price",
+					fieldname: "price_list_rate",
+					filters: {
+						price_list: frm.doc.price_list,
+						item_code: d.item_code
+					}
+				},
+				callback: function(r) {
+					let data = r.message;
+					if(data) {
+						console.log(d, data);
+						d.rate = data.price_list_rate
+						
+					}
+
+					frm.refresh_fields()
+				}
+			}
+		)
+	},
+	qty: function(frm, cdt, cdn) {
+		let d = locals[cdt][cdn]
+
+		frappe.call(
+			{
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Item Price",
+					fieldname: "price_list_rate",
+					filters: {
+						price_list: frm.doc.price_list,
+						item_code: d.item_code
+					}
+				},
+				callback: function(r) {
+					let data = r.message;
+					if(data) {
+						console.log(d, data);
+						d.rate = data.price_list_rate
+
+						let total_amount = d.qty * d.rate
+						d.amount = total_amount;
+						
+					}
+
+					frm.refresh_fields()
+				}
+			}
+		)
+
+
+		frm.refresh_fields()
+	}
+});	
