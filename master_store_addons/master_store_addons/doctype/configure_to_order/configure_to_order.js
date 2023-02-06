381744e2a28e3ba36cdf4f2002394a6a541b1d14
etms <etms@Ubunto22.04.1.myguest.virtualbox.org>
2023-02-06 03:04:58 -0800

    Filtering Child table linked field based on field in the same child table , Filtering On NOT Give The Same Last Name In Child Table

--- master_store_addons/master_store_addons/doctype/configure_to_order/configure_to_order.js
+++ master_store_addons/master_store_addons/doctype/configure_to_order/configure_to_order.js
@@ -1,45 +1,40 @@
 // Copyright (c) 2023, ebkar.ly and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on('Configure To Order', {
-// 	refresh: function(frm) {
-// 		frm.set_query('cto_option_details', function(){
-// 			return {
-// 				filters: {
-// 					cto_option: 0
-// 				}
-// 			}
-// 		})
-// 	},
-// });
 
 
-// frappe.ui.form.on("CTO Option Table", {
-// 	refresh: function(frm) {
-// 		cur_frm.set_query("cto_type", "cto_option_details",function(){
-// 			return {
-// 				filters: {
-// 					cto_type: "Display"
-// 				}
-				
-// 			}
-// 		})
-// 	},
-// 	cto_type: function(frm, cdt, cdn) {
-// 		let row = locals[cdt][cdn]
-// 		cur_frm.set_query("cto_type", "cto_optOptionOptionion_details",function(){
-// 			return {
-// 				filters: {
-// 					cto_type: "Display"
-// 				}
-				
-// 			}
-// 		})
-// 		console.log(row);
-// 	},
-// 	cto_option: function(frm,cdt,cdn){
-// 		let row = locals[cdt][cdn]
+//Filtering Child table linked field based on field in the same child table 
 
-		
-// 	}
-// })
+frappe.ui.form.on("Configure To Order", "refresh", function(frm) {
+	frm.fields_dict['cto_option_details'].grid.get_field('cto_option').get_query = function(doc, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		//console.log(child);
+		return {  
+			filters:[
+			['cto_type', '=', child.cto_type]
+			]
+		}
+		}
+	}
+);
+
+// Filtering On NOT Give The Same Last Name In Child Table
+frappe.ui.form.on("CTO Option Table", "cto_type", function(frm) {
+	frm.fields_dict['cto_option_details'].grid.get_field('cto_type').get_query = function(doc, cdt, cdn) {
+		var child = locals[cdt][cdn];
+		//console.log(child);
+		let inList = []
+		inList = frm.doc.cto_option_details.map(c =>{
+			if(c.cto_type) {
+				return c.cto_type;
+			}
+		})
+		return {  
+			filters:[
+				['cto_type', 'not in', inList]
+			]
+		}
+		}
+	}
+);
+	
